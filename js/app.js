6261ab4290b797f7245298aa662d1a16485f811e
Diego Go <diego.gomes87@gmail.com>
2019-12-14 21:38:15 -0300

    versao para test

--- js/app.js
+++ js/app.js
@@ -35,9 +35,63 @@ class UI {
     `
     list.appendChild(row);
   }
+
+  //show alerts
+  static showAlert(msg, className){
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(msg));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+
+    // Vanish in 3 seconds
+    setTimeout(() => document.querySelector('.alert').remove(), 3000);
+  }
+
+  //delete item in list
+  static deleteBook(el){
+    if(el.classList.contains('delete')){
+      el.parentElement.parentElement.remove();
+    }
+  }
+
+  // clear fieald
+  static clearFields(){
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#id').value = '';
+  }
 }
 // Store class localstorage
+class Store {
+  static getBooks(){
+    let books;
+    if(localStorage.getItem('books' === null)){
+      books = []
+    }else{
+      books = JSON.parse(localStorage.getItem('books'))
+    }
 
+    return books;
+  }
+  
+  static addBook(book){
+    const books = Store.getBooks();
+    books.push(book)
+    localStorage.setItem('books', JSON.stringify(books))
+  }
+
+  static removeBook(id){
+    const books =  Store.getBooks();
+    books.forEach((book, index) => {
+      if(book.id === id){
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books))
+  }
+}
 // event display book
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 // event listene
@@ -48,13 +102,30 @@ document.querySelector('#book-form').addEventListener('submit',(e)=>{
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
   const id = document.querySelector('#id').value;
-  
 
-  //instatiate book
-  const book = new Book(title,author,id);
-  console.log(book)
+  //validate
+  if(title === '' || author === '' || id === ''){
+    UI.showAlert('Preencha todos os Campos', 'danger');
+  }else {
+    //instatiate book
+    const book = new Book(title,author,id);
+    console.log(book)
 
-  // add book to UI
-  UI.addBookToList(book);
-})
+    // add book to UI
+    UI.addBookToList(book);
+
+    // clear fealds
+    UI.clearFields();
+    
+
+    // show success message
+    UI.showAlert('Adicionado a lista', 'success');
+  }
+});
 //event remove book
+document.querySelector('#book-list').addEventListener('click', (e)=>{
+  UI.deleteBook(e.target);
+
+  //delete item
+  UI.showAlert('Book Deleted', 'info')
+})
